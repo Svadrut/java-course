47fe5c380791ef4d25029fb56b83f8d305fe983e
Svadrut <svadrut@gmail.com>
2023-03-02 19:07:53 -0500

    Finished Twelfth tutorial

--- src/Twelfth/Twelfth.java
+++ src/Twelfth/Twelfth.java
@@ -9,7 +9,15 @@ class Person {
 
     // 1. Data
     // 2. Subroutines (methods)
-    //
+    void speak() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("My name is: " + name + " and I am " + age + " years old ");
+        }
+    }
+
+    void sayHello() {
+        System.out.println("Hello there!");
+    }
 }
 
 public class Twelfth {
@@ -18,10 +26,15 @@ public class Twelfth {
         Person person1 = new Person();
         person1.name = "Sam Smith";
         person1.age = 30;
+        person1.speak();
+        person1.sayHello();
 
         Person person2 = new Person();
         person2.name = "Harry Styles";
         person2.age = 29;
+        person2.speak();
+        person2.sayHello();
+
         System.out.println(person1.name);
 
     }
