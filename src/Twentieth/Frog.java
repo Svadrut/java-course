1923a252c05505d02a30cd26c552f3ca234f301c
Svadrut <svadrut@gmail.com>
2023-03-18 11:44:18 -0400

    Finished Twentieth.java

---
+++ src/Twentieth/Frog.java
@@ -0,0 +1,23 @@
+package Twentieth;
+
+public class Frog {
+    private int id;
+    private String name;
+
+    public Frog(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("%-4d: %s", id, name);
+
+        // StringBuilder sb = new StringBuilder();
+        // sb.append(id).append(": ").append(name);
+        // 
+        // return sb.toString();
+    }
+
+}
---
+++ src/Twentieth/Twentieth.java
@@ -0,0 +1,11 @@
+package Twentieth;
+
+public class Twentieth {
+    public static void main(String[] args) {
+        Frog frog1 = new Frog(7, "Freddy");
+        Frog frog2 = new Frog(5, "Roger");
+
+        System.out.println(frog1);
+        System.out.println(frog2);
+    }    
+}
