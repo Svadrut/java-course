7f1bac05be331001529fb0b734815caa873a2270
Svadrut <svadrut@gmail.com>
2023-03-21 20:42:37 -0400

    Finished TwentyFirst.java

---
+++ src/TwentyFirst/Car.java
@@ -0,0 +1,17 @@
+package TwentyFirst;
+
+public class Car extends Machine {
+
+    @Override
+    public void start() {
+        System.out.println("Car started.");
+    }
+
+    public void wipeWindSheild() {
+        System.out.println("Wiping wind sheild.");
+    }
+
+    public void showInfo() {
+        System.out.println("Car name: " + name);
+    }
+}
---
+++ src/TwentyFirst/Machine.java
@@ -0,0 +1,14 @@
+package TwentyFirst;
+
+public class Machine {
+
+    protected String name = "Machine type 1";
+
+    public void start() {
+        System.out.println("Machine started.");
+    }
+
+    public void stop() {
+        System.out.println("Machine stopped.");
+    }
+}
