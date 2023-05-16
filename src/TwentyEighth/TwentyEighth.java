80fdf113dd4a43dcfc777f757d1b16e8defb5da9
Svadrut <svadrut@gmail.com>
2023-05-16 16:35:47 -0400

    Finished TwentyEighth

---
+++ src/TwentyEighth/TwentyEighth.java
@@ -0,0 +1,31 @@
+package TwentyEighth;
+
+class Plant {
+    public static final int ID = 7;
+    private String name;
+
+    public String getData() {
+        String data = "some stuff" + calculateGrowthForecast();
+
+        return data;
+    }
+
+    private int calculateGrowthForecast() {
+        return 9;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+public class TwentyEighth {
+    public static void main(String[] args) {
+
+    }
+}
