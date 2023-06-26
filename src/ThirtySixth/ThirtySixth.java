5e4d6479fcd0864b0395f7288e8e124de5cab8a5
Svadrut <svadrut@gmail.com>
2023-06-26 18:17:42 -0400

    Finished ThirtySixth

---
+++ src/ThirtySixth/ThirtySixth.java
@@ -0,0 +1,45 @@
+package ThirtySixth;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+
+public class ThirtySixth {
+    public static void main(String[] args) {
+        Test test = new Test();
+
+        // try {
+        // test.run();
+        // } catch (IOException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // } catch (ParseException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+
+        // try {
+        // test.run();
+        // } catch (IOException | ParseException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+
+        try {
+            test.run();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            test.input();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
