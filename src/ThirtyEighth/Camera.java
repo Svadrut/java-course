87c32b69f185572f9a6cc7ad2cdf3fec5d2f3806
Svadrut <svadrut@gmail.com>
2023-06-28 13:57:35 -0400

    Finished ThirtyEighth

---
+++ src/ThirtyEighth/Camera.java
@@ -0,0 +1,22 @@
+package ThirtyEighth;
+
+public class Camera extends Machine {
+
+    @Override
+    public void start() {
+        System.out.println("Starting camera.");
+    }
+
+    @Override
+    public void doStuff() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void shutdown() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ src/ThirtyEighth/Car.java
@@ -0,0 +1,21 @@
+package ThirtyEighth;
+
+public class Car extends Machine {
+
+    @Override
+    public void start() {
+        System.out.println("Starting car.");
+    }
+
+    @Override
+    public void doStuff() {
+        System.out.println("Do stuff in car.");
+        
+    }
+
+    @Override
+    public void shutdown() {
+        System.out.println("Shutting down car.");
+    }
+    
+}
