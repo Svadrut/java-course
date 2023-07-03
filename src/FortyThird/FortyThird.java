ec66a778ac64d615a2a51414320c9db765ccc932
Svadrut <svadrut@gmail.com>
2023-07-03 16:45:06 -0400

    Finished FortyThird

---
+++ src/FortyThird/FortyThird.java
@@ -0,0 +1,15 @@
+package FortyThird;
+
+public class FortyThird {
+    public static void main(String[] args) {
+
+        Robot robot = new Robot(7);
+        robot.start();
+
+        // Robot.Brain brain = robot.new Brain();
+        // brain.think();
+
+        Robot.Battery battery = new Robot.Battery();
+        battery.charge();
+    }
+}
