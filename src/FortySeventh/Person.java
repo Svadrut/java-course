6abc7866183ad7acc7f2060181cf45be98373452
Svadrut <svadrut@gmail.com>
2023-07-12 10:59:41 -0400

    Finished FortySeventh

---
+++ src/FortySeventh/Person.java
@@ -0,0 +1,21 @@
+package FortySeventh;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 4801633306273802962L;
+    
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", name=" + name + "]";
+    }
+}
