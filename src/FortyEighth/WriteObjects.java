fecceada31e491adc0c50e3d6cac9986cbd98c51
Svadrut <svadrut@gmail.com>
2023-07-14 12:14:19 -0400

    Finished FortyEighth

---
+++ src/FortyEighth/WriteObjects.java
@@ -0,0 +1,26 @@
+package FortyEighth;
+
+import java.io.ObjectOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class WriteObjects {
+    public static void main(String[] args) {
+        System.out.println("Writing objects.....");
+
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("test.ser"))) {
+
+            Person person = new Person(7, "Bob");
+            Person.setCount(88);
+            os.writeObject(person);
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
